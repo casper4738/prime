bc8e73b37ecb53bf001edaa0071b167b7dd2dab2
dedy.suwandi <dedy.suwandi@011ACE0315.nu-ace.ad-ins.com>
2015-06-16 15:51:07 +0700

    Constants : -Konstanta untuk PAGE CHANGING :  a.PAGE_INSERT  b.PAGE_EDIT  c.PAGE_ADD  d.PAGE_UPDATE  e.PAGE_DELETE -Konstanta untuk DAY :  a.DAY_MONDAY  b.DAY_TUESDAY  c....  d.DAY_SUNDAY -Konstanta untuk STATUS :  a.STATUS_RECEIVE  b.STATUS_PROGRESS  c....  d.STATUS_ABORT

---
+++ prime01/src/prime/constants/Constants.java
@@ -0,0 +1,28 @@
+package prime.constants;
+
+public class Constants {
+	//##1.Standard Change Page - Action
+	public static Integer PAGE_ADD	  = 0;
+	public static Integer PAGE_INSERT = 1;
+	public static Integer PAGE_EDIT   = 2;
+	public static Integer PAGE_UPDATE = 3;
+	public static Integer PAGE_DELETE = 4;
+	
+	
+	//##2.Standard Status Value
+	public static Integer STATUS_RECEIVE  = 0;
+	public static Integer STATUS_PROGRESS = 1;
+	public static Integer STATUS_PAUSE    = 2;
+	public static Integer STATUS_SUBMIT   = 3;
+	public static Integer STATUS_FINISH   = 4;
+	public static Integer STATUS_ABORT    = 5;
+	
+	//##3.Standard Day Conversion
+	public static Integer DAY_MONDAY    = 0;
+	public static Integer DAY_TUESDAY   = 1;
+	public static Integer DAY_WEDNESDAY = 2;
+	public static Integer DAY_THURSDAY  = 3;
+	public static Integer DAY_FRIDAY 	= 4;
+	public static Integer DAY_SATURDAY  = 5;
+	public static Integer DAY_SUNDAY	= 6;
+}
