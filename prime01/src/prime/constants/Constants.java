add0e146bae9d25e09a309fe13f732890fcf9016
dedy.suwandi <dedy.suwandi@011ACE0315.nu-ace.ad-ins.com>
2015-06-16 16:08:48 +0700

    Membuang Konstanta PAGE_ACTION

--- prime01/src/prime/constants/Constants.java
+++ prime01/src/prime/constants/Constants.java
@@ -1,15 +1,7 @@
 package prime.constants;
 
 public class Constants {
-	//##1.Standard Change Page - Action
-	public static Integer PAGE_ADD	  = 0;
-	public static Integer PAGE_INSERT = 1;
-	public static Integer PAGE_EDIT   = 2;
-	public static Integer PAGE_UPDATE = 3;
-	public static Integer PAGE_DELETE = 4;
-	
-	
-	//##2.Standard Status Value
+	//##1.Standard Status Value
 	public static Integer STATUS_RECEIVE  = 0;
 	public static Integer STATUS_PROGRESS = 1;
 	public static Integer STATUS_PAUSE    = 2;
@@ -17,7 +9,7 @@ public class Constants {
 	public static Integer STATUS_FINISH   = 4;
 	public static Integer STATUS_ABORT    = 5;
 	
-	//##3.Standard Day Conversion
+	//##2.Standard Day Conversion
 	public static Integer DAY_MONDAY    = 0;
 	public static Integer DAY_TUESDAY   = 1;
 	public static Integer DAY_WEDNESDAY = 2;
