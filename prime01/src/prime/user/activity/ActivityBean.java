b83b882a1f6fa6be860708fba3bb6ca5ed180d35
felix <felix@013ACE0315.nu-ace.ad-ins.com>
2015-06-19 09:19:30 +0700

    felix

--- prime01/src/prime/user/activity/ActivityBean.java
+++ prime01/src/prime/user/activity/ActivityBean.java
@@ -5,6 +5,29 @@ public class ActivityBean {
 	private Integer taskId;
 	private String 	activityName;
 	private String 	activityDescription;
+	private Integer status;
+	private Integer taskAssigner;
+	private java.sql.Date changeDate;
+	
+	public Integer getTaskAssigner() {
+		return taskAssigner;
+	}
+	public void setTaskAssigner(Integer taskAssigner) {
+		this.taskAssigner = taskAssigner;
+	}
+	public java.sql.Date getChangeDate() {
+		return changeDate;
+	}
+	public void setChangeDate(java.sql.Date changeDate) {
+		this.changeDate = changeDate;
+	}
+	
+	public Integer getStatus() {
+		return status;
+	}
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
 	public Integer getActivityId() {
 		return activityId;
 	}
