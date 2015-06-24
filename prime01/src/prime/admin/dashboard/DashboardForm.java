c0ad41aedbdee4daf001a8778cd4407f7ed9c34f
adrian.dp <adrian.dp@005ACE0315.nu-ace.ad-ins.com>
2015-06-24 20:23:12 +0700

    adrian

--- prime01/src/prime/admin/dashboard/DashboardForm.java
+++ prime01/src/prime/admin/dashboard/DashboardForm.java
@@ -5,6 +5,7 @@ import org.apache.struts.action.ActionForm;
 
 public class DashboardForm extends ActionForm {
 private Integer year;
+private Integer  employeeId;
 
 public Integer getYear() {
 	return year;
@@ -12,6 +13,14 @@ public Integer getYear() {
 
 public void setYear(Integer year) {
 	this.year = year;
+}
+
+public Integer getEmployeeId() {
+	return employeeId;
+}
+
+public void setEmployeeId(Integer employeeId) {
+	this.employeeId = employeeId;
 }
 }
   
