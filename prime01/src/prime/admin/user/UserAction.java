21ad8c81b3c1a6f073634947969c69342af13861
Fandy Adam <casperadam91@gmail.com>
2015-06-26 11:53:54 +0700

    frans

--- prime01/src/prime/admin/user/UserAction.java
+++ prime01/src/prime/admin/user/UserAction.java
@@ -39,6 +39,7 @@ public class UserAction extends Action {
 			return mapping.findForward("edit");
 		} else if (Constants.Task.DOADD.equals(userForm.getTask())) {
 			userForm.getUserBean().setEmployeeId(userForm.getEmployeeId());
+			userForm.getUserBean().setUpdateBy("dedy");
 			tmpManager.insert(userForm.getUserBean());
 			return mapping.findForward("forward");
 		} else if(Constants.Task.DOLOCK.equals(userForm.getTask())) {
